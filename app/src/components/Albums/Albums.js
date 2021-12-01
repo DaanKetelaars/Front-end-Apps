f771348880ffde90a327c90715720da99efaf206
Daan Ketelaars <daanketelaars@gmail.com>
2021-12-01 22:53:44 +0100

    changes

---
+++ app/src/components/Albums/Albums.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+
+const Albums = () => {
+  const [albums, setAlbums] = useState([]);
+  const [result, setResults] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${result}&api_key=7ebc4d450f175fa75d3260763df487fb&format=json`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        let data = json;
+        let albumsArray = data.topalbums.album;
+        console.log(albumsArray);
+        albumsArray.sort((a, b) => b.playcount - a.playcount);
+        const size = 3;
+        const topThreeAlbums = albumsArray.slice(0, size);
+        setAlbums(topThreeAlbums);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [result]);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Search</label>
+          <input
+            type="text"
+            name="search"
+            placeholder="search for artist..."
+            onChange={(e) => setResults(e.target.value)}
+            value={result}
+          />
+        </div>
+      </form>
+      <div className="albumCovers">
+        {albums.map((album) => (
+          <article key={album.name}>
+            <ul>
+              <li>
+                <h3>{album.name}</h3>
+                <p>{album.playcount}</p>
+                <img
+                  src={album.image[3]["#text"]}
+                  alt={`Cover album of ${album.name}`}
+                />
+              </li>
+            </ul>
+          </article>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Albums;
---
+++ app/src/pages/Home/App.js
@@ -0,0 +1,16 @@
+// import
+import React from "react";
+import Albums from "../../components/Albums/Albums";
+
+function App() {
+  return (
+    <header>
+      <div className="container">
+        <h1>Top 3 Albums</h1>
+        <Albums />
+      </div>
+    </header>
+  );
+}
+
+export default App;
